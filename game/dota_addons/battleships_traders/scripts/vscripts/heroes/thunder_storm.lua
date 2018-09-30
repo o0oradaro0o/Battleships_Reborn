fa43d72f773dcaa0ea397f71e1705315ea5dd94e
VicFrank <matthewshattil@gmail.com>
2018-09-29 19:41:43 -0500

    reworked the stormchaser

--- game/dota_addons/battleships_traders/scripts/vscripts/heroes/thunder_storm.lua
+++ game/dota_addons/battleships_traders/scripts/vscripts/heroes/thunder_storm.lua
@@ -4,8 +4,6 @@ thunder_storm = class({})
 modifier_thunder_storm = class({})
 
 function modifier_thunder_storm:OnCreated()
-
-
     if IsServer() then
         self:StartIntervalThink(.4)
     end
@@ -25,12 +23,7 @@ function modifier_thunder_storm:GetEffectName()
   end
 
 function  modifier_thunder_storm:OnIntervalThink()
-	
-	
 	local caster =  self:GetAbility():GetCaster()
-	if RandomInt(0, 100)>caster:GetMana()/20+10 then
-		return
-	end
 	local sound = "Hero_Zuus.GodsWrath"
 	local ability =  self:GetAbility()
 	local lightning_radius = ability:GetSpecialValueFor("lightning_radius")
@@ -48,7 +41,7 @@ function  modifier_thunder_storm:OnIntervalThink()
       nil,
       lightning_bolt_search_radius,
       DOTA_UNIT_TARGET_TEAM_ENEMY,
-      DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+      DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_BUILDING,
       DOTA_UNIT_TARGET_FLAG_NO_INVIS,
       FIND_ANY_ORDER,
       false
@@ -67,17 +60,14 @@ function  modifier_thunder_storm:OnIntervalThink()
 end
 
 
-
-function thunder_storm:OnUpgrade()
+function thunder_storm:OnSpellStart()
 	local caster = self:GetCaster()
-	caster:AddNewModifier(self:GetCaster(), self, "modifier_thunder_storm", {})
-end
+  local ability = self
 
-function thunder_storm:OnOwnerSpawned()
-	local caster = self:GetCaster()
-	caster:AddNewModifier(self:GetCaster(), self, "modifier_thunder_storm", {})
-end
+  local duration = ability:GetSpecialValueFor("duration")
 
+	caster:AddNewModifier(self:GetCaster(), self, "modifier_thunder_storm", {duration = duration})
+end
 
 
 function thunder_storm:CastLightningBolt(ability, targetUnit, targetPoint)
@@ -86,9 +76,10 @@ function thunder_storm:CastLightningBolt(ability, targetUnit, targetPoint)
   local particle = "particles/basic_projectile/lightning_bolt.vpcf"
   local particleHeight = 2000
 
-  local sight_radius = 900--ability:GetSpecialValueFor("sight_radius")
+  local sight_radius = ability:GetSpecialValueFor("sight_radius")
   local sight_duration = ability:GetSpecialValueFor("sight_duration")
   local damage = ability:GetSpecialValueFor("damage")
+  local percent_health_as_damage = ability:GetSpecialValueFor("percent_health_as_damage")
 
   EmitSoundOn(sound, caster)
 	
@@ -97,7 +88,7 @@ function thunder_storm:CastLightningBolt(ability, targetUnit, targetPoint)
 	ParticleManager:SetParticleControl(particle, 1, targetPoint)
 	ParticleManager:SetParticleControl(particle, 2, targetPoint)
 
-	--create a dummy at the targeted location that has truesight
+	--create a dummy at the targeted location that provides vision
 	local unit = CreateUnitByName("dummy_vision10", targetPoint, false, caster, caster, caster:GetTeam())
 	unit:SetDayTimeVisionRange(sight_radius)
 	unit:SetNightTimeVisionRange(sight_radius)
@@ -108,18 +99,25 @@ function thunder_storm:CastLightningBolt(ability, targetUnit, targetPoint)
 	end)
 
 	if targetUnit then
+    if targetUnit:IsRealHero() then
+      caster:SetMana(caster:GetMana()+50)
+      damage = targetUnit:GetHealth() * percent_health_as_damage
+    else
+      caster:SetMana(caster:GetMana()+15)
+    end
+
+    if targetUnit:IsTower() then
+      damage = damage * .25
+    end
+
 		local damageTable = {
 			victim = targetUnit,
       attacker = caster, 
       damage = damage,
       damage_type = DAMAGE_TYPE_MAGICAL,
       ability = ability
-	}
-	if targetUnit:IsRealHero() then
-		caster:SetMana(caster:GetMana()+50)
-	else
-		caster:SetMana(caster:GetMana()+15)
-	end
+	  }
+	
     ApplyDamage(damageTable)
 	end
 end
