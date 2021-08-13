cf028d938a9e18208c33c4c7b2b76e68a31f8f0a
VicFrank <matthewshattil@gmail.com>
2021-08-12 18:15:29 -0600

    buffed reckless speed

--- game/battleships_traders/scripts/vscripts/heroes/ability_shard_giver.lua
+++ game/battleships_traders/scripts/vscripts/heroes/ability_shard_giver.lua
@@ -4,6 +4,7 @@ function ability_shard_giver:Spawn()
   if IsServer() then
     self:SetLevel(1)
     local caster = self:GetCaster()
+    print("give shard")
     caster:AddNewModifier(nil, nil, "modifier_item_aghanims_shard", {})
   end
 end
