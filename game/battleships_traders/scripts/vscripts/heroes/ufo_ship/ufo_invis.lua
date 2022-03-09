ae78263953f7ca9c33f1f954bb5e9d766058890c
VicFrank <matthewshattil@gmail.com>
2022-03-09 00:09:00 -0600

    fixed ufo invis

--- game/battleships_traders/scripts/vscripts/heroes/ufo_ship/ufo_invis.lua
+++ game/battleships_traders/scripts/vscripts/heroes/ufo_ship/ufo_invis.lua
@@ -1,119 +1,59 @@
 ufo_invis = class({})
 LinkLuaModifier( "modifier_ufo_invis", "heroes/ufo_ship/ufo_invis.lua", LUA_MODIFIER_MOTION_NONE )
 
---------------------------------------------------------------------------------
--- Passive Modifier
-function ufo_invis:GetIntrinsicModifierName()
-	return "modifier_ufo_invis"
-end
-
+function ufo_invis:GetIntrinsicModifierName() return "modifier_ufo_invis" end
 
 modifier_ufo_invis = class({})
 
---------------------------------------------------------------------------------
--- Classifications
-function modifier_ufo_invis:IsHidden()
-	-- dynamic
-	-- if IsServer() then
-		return not (self.invisOn and (not self:GetParent():PassivesDisabled()))
-	-- end
-end
+function modifier_ufo_invis:IsHidden() return not self:GetParent():HasModifier("modifier_invisible") end
+function modifier_ufo_invis:IsDebuff() return false end
+function modifier_ufo_invis:IsPurgable() return false end
 
-function modifier_ufo_invis:IsDebuff()
-	return false
-end
+function modifier_ufo_invis:OnCreated()
+	if not IsServer() then return end
 
-function modifier_ufo_invis:IsPurgable()
-	return false
-end
-
---------------------------------------------------------------------------------
--- Initializations
-function modifier_ufo_invis:OnCreated( kv )
-	-- references
-    self.radius = self:GetAbility():GetSpecialValueFor( "radius" )
-    self.cooldown  = self:GetAbility():GetSpecialValueFor( "cooldown" )
-	self.interval = 0.1
-
-	self.invisOn = true
-
-	-- Start interval
-	self:StartIntervalThink( self.interval )
+	self:StartIntervalThink(0.1)
 	self:OnIntervalThink()
 end
 
-function modifier_ufo_invis:OnRefresh( kv )
-	-- references
-    self.radius = self:GetAbility():GetSpecialValueFor( "radius" )
-    self.cooldown  = self:GetAbility():GetSpecialValueFor( "cooldown" )
-end
-
-function modifier_ufo_invis:OnDestroy( kv )
-
-end
-
---------------------------------------------------------------------------------
--- Modifier Effects
-function modifier_ufo_invis:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_EVASION_CONSTANT,
-	}
-
-	return funcs
-end
-
---------------------------------------------------------------------------------
-
-
---------------------------------------------------------------------------------
--- Interval Effects
 function modifier_ufo_invis:OnIntervalThink()
-	if IsServer() then
-		-- Hero search flag based on detecting or undetecting
-		local flag = 0
-		if self.invisOn then
-			flag = DOTA_UNIT_TARGET_FLAG_NO_INVIS
-		else
-			flag = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_INVULNERABLE
+	if not IsServer() then return end
+
+	local caster = self:GetCaster()
+	local ability = self:GetAbility()
+	local cooldown = ability:GetCooldown(ability:GetLevel())
+	local enemies = FindUnitsInRadius(
+		self:GetParent():GetTeamNumber(),	-- int, your team number
+		self:GetParent():GetOrigin(),	-- point, center point
+		nil,	-- handle, cacheUnit. (not known)
+		self:GetAbility():GetSpecialValueFor( "radius" ),	-- float, radius. or use FIND_UNITS_EVERYWHERE
+		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
+		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_BUILDING,	-- int, type filter
+		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,	-- int, flag filter
+		0,	-- int, order filter
+		false	-- bool, can grow cache
+	)
+
+	if #enemies > 0 then
+		if caster:HasModifier("modifier_invisible") then
+			caster:RemoveModifierByName("modifier_invisible")
 		end
+		if ability:IsActivated() then
+			ability:SetActivated(false)
+			ability:EndCooldown()
+		end
+	else
+		if not ability:IsActivated() then
+			ability:SetActivated(true)
+			ability:StartCooldown(cooldown)
+		end
+	end
 
-		-- Find Enemy Heroes in Radius
-		local enemies = FindUnitsInRadius(
-			self:GetParent():GetTeamNumber(),	-- int, your team number
-			self:GetParent():GetOrigin(),	-- point, center point
-			nil,	-- handle, cacheUnit. (not known)
-			self.radius,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-			flag,	-- int, flag filter
-			0,	-- int, order filter
-			false	-- bool, can grow cache
-		)
-
-		-- Flip if detected
-        if (self.invisOn) == (#enemies>0) then
-            self:GetCaster():RemoveModifierByName("modifier_invisible")
-            self:GetAbility():StartCooldown(self.cooldown)
-        elseif not self:GetCaster():HasModifier("modifier_invisible") and self:GetAbility():GetCooldownTimeRemaining()==0.5 then
-            local particleName = "particles/econ/items/faceless_void/faceless_void_bracers_of_aeons/fv_bracers_of_aeons_dialatedebuf_hex.vpcf"
-            local particle = ParticleManager:CreateParticle(particleName, PATTACH_WORLDORIGIN, caster)
-            ParticleManager:SetParticleControl(particle, 0, caster:GetAbsOrigin())
-            ParticleManager:SetParticleControl(particle, 1, caster:GetAbsOrigin())
-            ParticleManager:SetParticleControl(particle, 2, Vector(explosion_radius, 1, 1))
-            ParticleManager:ReleaseParticleIndex(particle)
-        elseif not self:GetCaster():HasModifier("modifier_invisible") and self:GetAbility():GetCooldownTimeRemaining()==0 then
-            self:GetCaster():AddNewModifier(self:GetCaster(),nil, "modifier_invisible", {})
-        end
+	if ability:IsCooldownReady() and ability:IsActivated() and not caster:HasModifier("modifier_invisible") then
+		caster:AddNewModifier(self:GetCaster(), self, "modifier_invisible", {})
 	end
 end
 
---------------------------------------------------------------------------------
--- Graphics & Animations
 function modifier_ufo_invis:GetEffectName()
-	return "particles/units/heroes/hero_phantom_assassin/phantom_assassin_blur.vpcf"
+	return "particles/units/heroes/hero_templar_assassin/templar_assassin_meld.vpcf"
 end
-
-function modifier_ufo_invis:GetEffectAttachType()
-	return PATTACH_ABSORIGIN_FOLLOW
-end
-
