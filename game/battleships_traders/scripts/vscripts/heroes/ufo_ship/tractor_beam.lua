a62364ab926edf48b501c48fd4f0acb336a57b5c
VicFrank <matthewshattil@gmail.com>
2022-03-27 22:31:33 -0500

    Cleaned up tractor beam

--- game/battleships_traders/scripts/vscripts/heroes/ufo_ship/tractor_beam.lua
+++ game/battleships_traders/scripts/vscripts/heroes/ufo_ship/tractor_beam.lua
@@ -3,35 +3,30 @@ tractor_beam = tractor_beam or class({})
 LinkLuaModifier("modifier_tractor_beam", "heroes/ufo_ship/tractor_beam", LUA_MODIFIER_MOTION_NONE)
 
 function tractor_beam:OnSpellStart()
-
   if not IsServer() then return end
+
   local caster = self:GetCaster()
   local ability = self
+  local target = self:GetCursorPosition()
 
   local duration = ability:GetSpecialValueFor("duration")
 
+  local direction = (target - caster:GetAbsOrigin()):Normalized()
 
-  local vPos = nil
-  if self:GetCursorTarget() then
-    vPos = self:GetCursorTarget():GetOrigin()
-  else
-    vPos = self:GetCursorPosition()
-  end
-  self.vPos = vPos
-  local vDirection = vPos - caster:GetAbsOrigin()
-  vDirection.z = 0.0
-  vDirection = vDirection:Normalized()
-  caster:AddNewModifier(caster, ability, "modifier_tractor_beam", {duration = duration})
-  local particle = "particles/ufo_pull.vpcf"
-  local sound = "Hero_DragonKnight.BreathFire"
-
-  local particleName = "particles/ufo_pull.vpcf"
-  self.particle = ParticleManager:CreateParticle(particleName, PATTACH_WORLDORIGIN, caster)
-  ParticleManager:SetParticleControl(self.particle, 0, caster:GetAbsOrigin())
-  ParticleManager:SetParticleControl(self.particle, 1, vPos)
-
+  caster:AddNewModifier(caster, ability, "modifier_tractor_beam", {
+    direction_x = direction.x,
+    direction_y = direction.y
+  })
 end
 
+function tractor_beam:OnChannelFinish(interrupted)
+  if not IsServer() then return end
+
+  local caster = self:GetCaster()
+  local ability = self
+
+  caster:RemoveModifierByName("modifier_tractor_beam")
+end
 
 modifier_tractor_beam = class({})
 
@@ -39,74 +34,64 @@ function modifier_tractor_beam:IsHidden() return false end
 function modifier_tractor_beam:IsPurgable() return false end
 function modifier_tractor_beam:IsDebuff() return false end
 
-function modifier_tractor_beam:OnCreated(params)
+function modifier_tractor_beam:OnCreated(keys)
   if not IsServer() then return end
-  self.caster =   self:GetCaster()
-  self:StartIntervalThink(0.2)
+  self.caster = self:GetCaster()
+  self.direction = Vector(keys.direction_x, keys.direction_y, 0)
+
+  local target = self.caster:GetAbsOrigin() + self.direction * self:GetAbility():GetSpecialValueFor("end_radius")
+
+  self:StartIntervalThink(1/30)
 	self:OnIntervalThink()
+
+  local particleName = "particles/ufo_pull.vpcf"
+  self.particle = ParticleManager:CreateParticle(particleName, PATTACH_WORLDORIGIN, self.caster)
+  ParticleManager:SetParticleControl(self.particle, 0, self.caster:GetAbsOrigin())
+  ParticleManager:SetParticleControl(self.particle, 1, target)
 end
 
-localCurInDegrees=0;
 
 function modifier_tractor_beam:OnIntervalThink()
   local caster = self.caster
   local casterPosition = caster:GetAbsOrigin()
+  local range = self:GetAbility():GetSpecialValueFor("range")
+  local suck_speed = 1.5
+  local target = self.target
 
-  fire(self.vPos, self)
-end
-function modifier_tractor_beam:OnDestroy(params)
   if not IsServer() then return end
-  print( self.particle .. " was the particle index")
-  ParticleManager:DestroyParticle(self.particle,true)
 
-end
+  local enemies = FindUnitsInRadius(
+    caster:GetTeamNumber(),
+    casterPosition,
+    nil,
+    range,
+    DOTA_UNIT_TARGET_TEAM_ENEMY,
+    DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+    DOTA_UNIT_TARGET_FLAG_NONE,
+    FIND_ANY_ORDER,
+    false
+  )
 
-function fire(vPos, selfptr)
-  local casterPosition = selfptr.caster:GetAbsOrigin()
-  local particle = "particles/deathbeam.vpcf"
-  local sound = "Hero_DragonKnight.BreathFire"
+  -- get enemies in a cone
+  for _,enemy in pairs(enemies) do
+    local enemyPosition = enemy:GetAbsOrigin()
 
-  local start_radius = selfptr:GetAbility():GetSpecialValueFor( "start_radius" )
-  local end_radius = selfptr:GetAbility():GetSpecialValueFor( "end_radius" )
-  local speed = selfptr:GetAbility():GetSpecialValueFor( "speed" )
-  local range = selfptr:GetAbility():GetSpecialValueFor("range")
+    -- suck the target towards the caster
+    local distance = (enemyPosition - casterPosition):Length2D()
+    local enemyDirection = (enemyPosition - casterPosition):Normalized()
 
-  EmitSoundOn(sound, caster)
+    -- get the difference in degrees between direction and enemy direction
+    local angle = math.deg(math.acos(enemyDirection:Dot(self.direction)))
 
-  local vDirection = vPos - casterPosition
-  vDirection.z = 0.0
-  vDirection = vDirection:Normalized()
-
-  local projectile = {
-    Ability = selfptr:GetAbility(),
-    EffectName = particle,
-    vSpawnOrigin = selfptr.caster:GetAbsOrigin(),
-    fDistance = range,
-    fStartRadius = start_radius,
-    fEndRadius = end_radius,
-    Source = selfptr.caster,
-    bHasFrontalCone = false,
-    bReplaceExisting = false,
-    iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
-    iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
-    bDeleteOnHit = false,
-    vVelocity = vDirection * speed,
-    bProvidesVision = false,
-  }
-
-  ProjectileManager:CreateLinearProjectile(projectile)
-end
-
-function tractor_beam:OnProjectileHit( hTarget, vLocation )
-  if IsServer() then
-    local damage = self:GetSpecialValueFor( "damage" )
-
-    if not hTarget then
-      return nil
+    -- pull the enemy towards the caster
+    if angle < 45 then
+      local newPosition = enemyPosition + enemyDirection * (distance - range) * (1 - angle / 90) * suck_speed * FrameTime()
+      FindClearSpaceForUnit(enemy, newPosition, false)
     end
-    
-    
-
-    ApplyDamage( damage )
   end
+end
+
+function modifier_tractor_beam:OnDestroy()
+  if not (self.particle) then return end
+  ParticleManager:DestroyParticle(self.particle, true)
 end
