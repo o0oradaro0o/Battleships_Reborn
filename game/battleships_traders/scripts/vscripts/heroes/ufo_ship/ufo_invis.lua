0c360c3df23a64803d952625bcea1923edf965b8
o0oradaro0o <l.vanatta@gmail.com>
2022-03-28 19:24:04 -0500

    add flying to invis

--- game/battleships_traders/scripts/vscripts/heroes/ufo_ship/ufo_invis.lua
+++ game/battleships_traders/scripts/vscripts/heroes/ufo_ship/ufo_invis.lua
@@ -4,7 +4,7 @@ LinkLuaModifier( "modifier_ufo_invis", "heroes/ufo_ship/ufo_invis.lua", LUA_MODI
 function ufo_invis:GetIntrinsicModifierName() return "modifier_ufo_invis" end
 
 modifier_ufo_invis = class({})
-
+canfly=false;
 function modifier_ufo_invis:IsHidden() return not self:GetParent():HasModifier("modifier_invisible") end
 function modifier_ufo_invis:IsDebuff() return false end
 function modifier_ufo_invis:IsPurgable() return false end
@@ -15,6 +15,13 @@ function modifier_ufo_invis:OnCreated()
 	self:StartIntervalThink(0.1)
 	self:OnIntervalThink()
 end
+
+function modifier_ufo_invis:CheckState()
+	return {
+	  [MODIFIER_STATE_FLYING] = canfly,
+	}
+  end
+  
 
 function modifier_ufo_invis:OnIntervalThink()
 	if not IsServer() then return end
@@ -36,6 +43,7 @@ function modifier_ufo_invis:OnIntervalThink()
 
 	if #enemies > 0 then
 		if caster:HasModifier("modifier_invisible") then
+			canfly=false;
 			caster:RemoveModifierByName("modifier_invisible")
 		end
 		if ability:IsActivated() then
@@ -51,6 +59,7 @@ function modifier_ufo_invis:OnIntervalThink()
 
 	if ability:IsCooldownReady() and ability:IsActivated() and not caster:HasModifier("modifier_invisible") then
 		caster:AddNewModifier(self:GetCaster(), self, "modifier_invisible", {})
+		canfly=true;
 	end
 end
 
